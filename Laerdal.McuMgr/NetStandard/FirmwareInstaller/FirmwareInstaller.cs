fc03c9c7cc02eec4d13b130d7c217cf08811728b
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2023-08-24 16:52:50 +0200

    refa (iOS/FirmwareInstaller): refactor the implementation to align it with the new mechanism

--- Laerdal.McuMgr/NetStandard/FirmwareInstaller/FirmwareInstaller.cs
+++ Laerdal.McuMgr/NetStandard/FirmwareInstaller/FirmwareInstaller.cs
@@ -11,24 +11,5 @@ namespace Laerdal.McuMgr.FirmwareInstaller
     public partial class FirmwareInstaller : IFirmwareInstaller
     {
         public FirmwareInstaller(object bluetoothDevice) => throw new NotImplementedException();
-
-        // public string LastFatalErrorMessage => throw new NotImplementedException();
-        //
-        // public EFirmwareInstallationVerdict BeginInstallation(
-        //     byte[] data,
-        //     EFirmwareInstallationMode mode = EFirmwareInstallationMode.TestAndConfirm,
-        //     bool? eraseSettings = null,
-        //     int? estimatedSwapTimeInMilliseconds = null,
-        //     int? windowCapacity = null,
-        //     int? memoryAlignment = null,
-        //     int? pipelineDepth = null,
-        //     int? byteAlignment = null
-        // )
-        // {
-        //     throw new NotImplementedException();
-        // }
-        //
-        // public void Cancel() => throw new NotImplementedException();
-        // public void Disconnect() => throw new NotImplementedException();
     }
 }
--- Laerdal.McuMgr/iOS/FirmwareInstaller/FirmwareInstaller.cs
+++ Laerdal.McuMgr/iOS/FirmwareInstaller/FirmwareInstaller.cs
@@ -7,7 +7,7 @@ using Foundation;
 using Laerdal.McuMgr.Common;
 using Laerdal.McuMgr.FirmwareInstaller.Contracts;
 using Laerdal.McuMgr.FirmwareInstaller.Contracts.Enums;
-using Laerdal.McuMgr.FirmwareInstaller.Contracts.Events;
+using Laerdal.McuMgr.FirmwareInstaller.Contracts.Native;
 using McuMgrBindingsiOS;
 
 namespace Laerdal.McuMgr.FirmwareInstaller
@@ -15,110 +15,125 @@ namespace Laerdal.McuMgr.FirmwareInstaller
     /// <inheritdoc cref="IFirmwareInstaller"/>
     public partial class FirmwareInstaller : IFirmwareInstaller
     {
-        private readonly IOSFirmwareInstaller _iosFirmwareInstallerProxy;
-
-        public FirmwareInstaller(CBPeripheral bluetoothDevice)
+        public FirmwareInstaller(CBPeripheral bluetoothDevice) : this(ValidateArgumentsAndConstructProxy(bluetoothDevice))
         {
-            if (bluetoothDevice == null)
-                throw new ArgumentNullException(nameof(bluetoothDevice));
+        }
 
-            _iosFirmwareInstallerProxy = new IOSFirmwareInstaller(
-                listener: new IOSFirmwareInstallerListenerProxy(this),
-                cbPeripheral: bluetoothDevice
+        static private INativeFirmwareInstallerProxy ValidateArgumentsAndConstructProxy(CBPeripheral bluetoothDevice)
+        {
+            bluetoothDevice = bluetoothDevice ?? throw new ArgumentNullException(nameof(bluetoothDevice));
+
+            return new IOSNativeFirmwareInstallerProxy(
+                bluetoothDevice: bluetoothDevice,
+                nativeFirmwareInstallerCallbacksProxy: new GenericNativeFirmwareInstallerCallbacksProxy()
             );
         }
 
-        // public EFirmwareInstallationVerdict BeginInstallation(
-        //     byte[] data,
-        //     EFirmwareInstallationMode mode = EFirmwareInstallationMode.TestAndConfirm,
-        //     bool? eraseSettings = null,
-        //     int? estimatedSwapTimeInMilliseconds = null,
-        //     int? windowCapacity = null, //not applicable in ios
-        //     int? memoryAlignment = null, //not applicable in ios
-        //     int? pipelineDepth = null,
-        //     int? byteAlignment = null
-        // )
-        // {
-        //     //todo   nsdata should be tracked in a private variable and then cleaned up properly   currently it might get disposed ahead of time
-        //     var nsData = NSData.FromArray(data);
-        //
-        //     var verdict = _iosFirmwareInstallerProxy.BeginInstallation(
-        //         mode: TranslateFirmwareInstallationMode(mode),
-        //         imageData: nsData,
-        //         eraseSettings: eraseSettings ?? false,
-        //         // windowCapacity: not applicable in ios,
-        //         // memoryAlignment: not applicable in ios,
-        //         pipelineDepth: pipelineDepth ?? -1,
-        //         byteAlignment: byteAlignment ?? -1,
-        //         estimatedSwapTimeInMilliseconds: estimatedSwapTimeInMilliseconds ?? -1
-        //     );
-        //
-        //     return TranslateFirmwareInstallationVerdict(verdict);
-        // }
-
-        // public string LastFatalErrorMessage => _iosFirmwareInstallerProxy?.LastFatalErrorMessage;
-        //
-        // public void Cancel() => _iosFirmwareInstallerProxy.Cancel();
-        // public void Disconnect() => _iosFirmwareInstallerProxy.Disconnect();
-
-        static private EIOSFirmwareInstallationMode TranslateFirmwareInstallationMode(EFirmwareInstallationMode mode) => mode switch
-        {
-            EFirmwareInstallationMode.TestOnly => EIOSFirmwareInstallationMode.TestOnly, //0
-            EFirmwareInstallationMode.ConfirmOnly => EIOSFirmwareInstallationMode.ConfirmOnly,
-            EFirmwareInstallationMode.TestAndConfirm => EIOSFirmwareInstallationMode.TestAndConfirm,
-            _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
-
-            //0 we have to separate enums
-            //
-            //  - EFirmwareInstallationMode which is publicly exposed and used by both android and ios
-            //  - EIOSFirmwareInstallationMode which is specific to ios and should not be used by the api surface or the end users  
-        };
-
-        static private EFirmwareInstallationVerdict TranslateFirmwareInstallationVerdict(EIOSFirmwareInstallationVerdict verdict) => verdict switch
-        {
-            EIOSFirmwareInstallationVerdict.Success => EFirmwareInstallationVerdict.Success, //0
-            EIOSFirmwareInstallationVerdict.FailedDeploymentError => EFirmwareInstallationVerdict.FailedDeploymentError,
-            EIOSFirmwareInstallationVerdict.FailedInvalidSettings => EFirmwareInstallationVerdict.FailedInvalidSettings,
-            EIOSFirmwareInstallationVerdict.FailedInvalidDataFile => EFirmwareInstallationVerdict.FailedInvalidDataFile,
-            EIOSFirmwareInstallationVerdict.FailedInstallationAlreadyInProgress => EFirmwareInstallationVerdict.FailedInstallationAlreadyInProgress,
-            _ => throw new ArgumentOutOfRangeException(nameof(verdict), verdict, null)
-
-            //0 we have to separate enums
-            //
-            //  - EFirmwareInstallationVerdict which is publicly exposed and used by both android and ios
-            //  - EIOSFirmwareInstallationVerdict which is specific to ios and should not be used by the api surface or the end users
-        };
-
         // ReSharper disable once InconsistentNaming
-        private sealed class IOSFirmwareInstallerListenerProxy : IOSListenerForFirmwareInstaller
+        private sealed class IOSNativeFirmwareInstallerProxy : IOSListenerForFirmwareInstaller, INativeFirmwareInstallerProxy
         {
-            private readonly FirmwareInstaller _installer;
+            private readonly IOSFirmwareInstaller _nativeIosFirmwareInstaller;
+            private readonly INativeFirmwareInstallerCallbacksProxy _nativeFirmwareInstallerCallbacksProxy;
 
-            internal IOSFirmwareInstallerListenerProxy(FirmwareInstaller installer)
+            public string Nickname { get; set; }
+            
+            public IFirmwareInstallerEventEmittable FirmwareInstaller { get; set; }
+
+            internal IOSNativeFirmwareInstallerProxy(CBPeripheral bluetoothDevice, INativeFirmwareInstallerCallbacksProxy nativeFirmwareInstallerCallbacksProxy)
             {
-                _installer = installer ?? throw new ArgumentNullException(nameof(installer));
+                bluetoothDevice = bluetoothDevice ?? throw new ArgumentNullException(nameof(bluetoothDevice));
+                nativeFirmwareInstallerCallbacksProxy = nativeFirmwareInstallerCallbacksProxy ?? throw new ArgumentNullException(nameof(nativeFirmwareInstallerCallbacksProxy));
+
+                _nativeIosFirmwareInstaller = new IOSFirmwareInstaller(listener: this, cbPeripheral: bluetoothDevice);
+                _nativeFirmwareInstallerCallbacksProxy = nativeFirmwareInstallerCallbacksProxy; //composition-over-inheritance
             }
 
-            public override void CancelledAdvertisement() => _installer.OnCancelled(new CancelledEventArgs());
-            public override void BusyStateChangedAdvertisement(bool busyNotIdle) => _installer.OnBusyStateChanged(new BusyStateChangedEventArgs(busyNotIdle));
-            public override void FatalErrorOccurredAdvertisement(string errorMessage) => _installer.OnFatalErrorOccurred(new FatalErrorOccurredEventArgs(errorMessage));
 
-            public override void LogMessageAdvertisement(string message, string category, string level) => _installer.OnLogEmitted(new LogEmittedEventArgs(
-                level: HelpersIOS.TranslateEIOSLogLevel(level),
-                message: message,
-                category: category,
-                resource: "firmware-installer"
-            ));
+
+            #region commands
+
+            public string LastFatalErrorMessage => _nativeIosFirmwareInstaller?.LastFatalErrorMessage;
+
+            public void Cancel() => _nativeIosFirmwareInstaller?.Cancel();
+            public void Disconnect() => _nativeIosFirmwareInstaller?.Disconnect();
+
+            public EFirmwareInstallationVerdict BeginInstallation(
+                byte[] data,
+                EFirmwareInstallationMode mode = EFirmwareInstallationMode.TestAndConfirm,
+                bool? eraseSettings = null,
+                int? estimatedSwapTimeInMilliseconds = null,
+                int? windowCapacity = null, //not applicable in ios
+                int? memoryAlignment = null, //not applicable in ios
+                int? pipelineDepth = null,
+                int? byteAlignment = null
+            )
+            {
+                //todo   nsdata should be tracked in a private variable and then cleaned up properly   currently it might get disposed ahead of time
+                var nsData = NSData.FromArray(data);
+
+                var verdict = _nativeIosFirmwareInstaller.BeginInstallation(
+                    mode: TranslateFirmwareInstallationMode(mode),
+                    imageData: nsData,
+                    eraseSettings: eraseSettings ?? false,
+                    pipelineDepth: pipelineDepth ?? -1,
+                    byteAlignment: byteAlignment ?? -1,
+                    estimatedSwapTimeInMilliseconds: estimatedSwapTimeInMilliseconds ?? -1
+                );
+
+                return TranslateFirmwareInstallationVerdict(verdict);
+            }
+
+            #endregion commands
+
+
+
+            #region ios listener callbacks -> csharp event emitters
             
-            public override void StateChangedAdvertisement(EIOSFirmwareInstallationState oldState, EIOSFirmwareInstallationState newState) => _installer.OnStateChanged(new StateChangedEventArgs(
-                newState: TranslateEIOSFirmwareInstallationState(newState),
-                oldState: TranslateEIOSFirmwareInstallationState(oldState)
-            ));
+            public override void CancelledAdvertisement() => _nativeFirmwareInstallerCallbacksProxy?.CancelledAdvertisement();
+            public override void BusyStateChangedAdvertisement(bool busyNotIdle) => _nativeFirmwareInstallerCallbacksProxy?.BusyStateChangedAdvertisement(busyNotIdle);
+            public override void FatalErrorOccurredAdvertisement(string errorMessage) => _nativeFirmwareInstallerCallbacksProxy?.FatalErrorOccurredAdvertisement(errorMessage);
 
-            public override void FirmwareUploadProgressPercentageAndThroughputDataChangedAdvertisement(nint progressPercentage, float averageThroughput) => _installer.OnFirmwareUploadProgressPercentageAndThroughputDataChanged(new FirmwareUploadProgressPercentageAndDataThroughputChangedEventArgs(
-                averageThroughput: averageThroughput,
-                progressPercentage: (int)progressPercentage
-            ));
+            public override void LogMessageAdvertisement(string message, string category, string level)
+                => LogMessageAdvertisement(
+                    level: HelpersIOS.TranslateEIOSLogLevel(level),
+                    message: message,
+                    category: category,
+                    resource: Nickname
+                );
+
+            public void LogMessageAdvertisement(string message, string category, ELogLevel level, string resource) //just to conform to the interface
+                => _nativeFirmwareInstallerCallbacksProxy?.LogMessageAdvertisement(
+                    level: level,
+                    message: message,
+                    category: category,
+                    resource: resource
+                );
+
+            public override void StateChangedAdvertisement(EIOSFirmwareInstallationState oldState, EIOSFirmwareInstallationState newState)
+                => StateChangedAdvertisement(
+                    newState: TranslateEIOSFirmwareInstallationState(newState),
+                    oldState: TranslateEIOSFirmwareInstallationState(oldState)
+                );
+
+            public void StateChangedAdvertisement(EFirmwareInstallationState oldState, EFirmwareInstallationState newState) //just to conform to the interface
+                => _nativeFirmwareInstallerCallbacksProxy?.StateChangedAdvertisement(
+                    newState: newState,
+                    oldState: oldState
+                );
+
+            // todo rename this to FirmwareUploadProgressPercentageAndDataThroughputChangedAdvertisement
+            public override void FirmwareUploadProgressPercentageAndThroughputDataChangedAdvertisement(nint progressPercentage, float averageThroughput)
+                => FirmwareUploadProgressPercentageAndDataThroughputChangedAdvertisement((int)progressPercentage, averageThroughput);
+            
+            public void FirmwareUploadProgressPercentageAndDataThroughputChangedAdvertisement(int progressPercentage, float averageThroughput) //just to conform to the interface
+                => _nativeFirmwareInstallerCallbacksProxy?.FirmwareUploadProgressPercentageAndDataThroughputChangedAdvertisement(
+                    averageThroughput: averageThroughput,
+                    progressPercentage: progressPercentage
+                );
+            
+            #endregion
+
+
 
             // ReSharper disable once InconsistentNaming
             static private EFirmwareInstallationState TranslateEIOSFirmwareInstallationState(EIOSFirmwareInstallationState state) => state switch
@@ -135,6 +150,35 @@ namespace Laerdal.McuMgr.FirmwareInstaller
                 EIOSFirmwareInstallationState.Validating => EFirmwareInstallationState.Validating,
                 EIOSFirmwareInstallationState.Confirming => EFirmwareInstallationState.Confirming,
                 _ => throw new ArgumentOutOfRangeException(nameof(state), state, null)
+            };
+            
+            
+            static private EIOSFirmwareInstallationMode TranslateFirmwareInstallationMode(EFirmwareInstallationMode mode) => mode switch
+            {
+                EFirmwareInstallationMode.TestOnly => EIOSFirmwareInstallationMode.TestOnly, //0
+                EFirmwareInstallationMode.ConfirmOnly => EIOSFirmwareInstallationMode.ConfirmOnly,
+                EFirmwareInstallationMode.TestAndConfirm => EIOSFirmwareInstallationMode.TestAndConfirm,
+                _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
+
+                //0 we have to separate enums
+                //
+                //  - EFirmwareInstallationMode which is publicly exposed and used by both android and ios
+                //  - EIOSFirmwareInstallationMode which is specific to ios and should not be used by the api surface or the end users  
+            };
+
+            static private EFirmwareInstallationVerdict TranslateFirmwareInstallationVerdict(EIOSFirmwareInstallationVerdict verdict) => verdict switch
+            {
+                EIOSFirmwareInstallationVerdict.Success => EFirmwareInstallationVerdict.Success, //0
+                EIOSFirmwareInstallationVerdict.FailedDeploymentError => EFirmwareInstallationVerdict.FailedDeploymentError,
+                EIOSFirmwareInstallationVerdict.FailedInvalidSettings => EFirmwareInstallationVerdict.FailedInvalidSettings,
+                EIOSFirmwareInstallationVerdict.FailedInvalidDataFile => EFirmwareInstallationVerdict.FailedInvalidDataFile,
+                EIOSFirmwareInstallationVerdict.FailedInstallationAlreadyInProgress => EFirmwareInstallationVerdict.FailedInstallationAlreadyInProgress,
+                _ => throw new ArgumentOutOfRangeException(nameof(verdict), verdict, null)
+
+                //0 we have to separate enums
+                //
+                //  - EFirmwareInstallationVerdict which is publicly exposed and used by both android and ios
+                //  - EIOSFirmwareInstallationVerdict which is specific to ios and should not be used by the api surface or the end users
             };
         }
     }
