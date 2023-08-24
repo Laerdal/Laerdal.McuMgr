cb1b9c7cbf27d7b1b1a3eebd840bc7d05dc90054
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2023-08-24 13:33:33 +0200

    doc (IFirmwareInstallerCommandable.cs): trivial xml-doc tweaks
    [skip ci]

--- Laerdal.McuMgr/Shared/FirmwareInstaller/Contracts/IFirmwareInstallerCommandable.cs
+++ Laerdal.McuMgr/Shared/FirmwareInstaller/Contracts/IFirmwareInstallerCommandable.cs
@@ -13,7 +13,7 @@ namespace Laerdal.McuMgr.FirmwareInstaller.Contracts
         /// <param name="eraseSettings">Specifies whether preexisting settings should be erased or not.</param>
         /// <param name="estimatedSwapTimeInMilliseconds">In rF52840, due to how the flash memory works, requires ~20 sec to erase images.
         ///     For Laerdal AEDs the recommended time is ~50secs. Adjust the time accordingly for each device you're testing.</param>
-        /// <param name="windowCapacity">Set the window capacity. Values > 1 enable a new implementation for uploading
+        /// <param name="windowCapacity">(Android only) Set the window capacity. Values > 1 enable a new implementation for uploading
         ///     the images, which makes use of SMP pipelining feature. The app will send this many packets immediately, without waiting for notification
         ///     confirming each packet. This value should be lower or equal to MCUMGR_BUF_COUNT
         ///     (https://github.com/zephyrproject-rtos/zephyr/blob/bd4ddec0c8c822bbdd420bd558b62c1d1a532c16/subsys/mgmt/mcumgr/Kconfig#L550)
@@ -21,10 +21,10 @@ namespace Laerdal.McuMgr.FirmwareInstaller.Contracts
         ///     before https://github.com/zephyrproject-rtos/zephyr/pull/41959 was merged, the device required data to be sent with memory alignment.
         ///     Otherwise, the device would ignore uneven bytes and reply with lower than expected offset
         ///     causing multiple packets to be sent again dropping the speed instead of increasing it.</param>
-        /// <param name="memoryAlignment">Set the selected memory alignment. Defaults to 4 to match Nordic devices.</param>
-        /// <param name="pipelineDepth">If set to a value larger than 1, this enables SMP Pipelining, wherein multiple packets of data ('chunks') are sent at
+        /// <param name="memoryAlignment">(Android only) Set the selected memory alignment. Defaults to 4 to match Nordic devices.</param>
+        /// <param name="pipelineDepth">(iOS only) If set to a value larger than 1, this enables SMP Pipelining, wherein multiple packets of data ('chunks') are sent at
         ///     once before awaiting a response, which can lead to a big increase in transfer speed if the receiving hardware supports this feature.</param>
-        /// <param name="byteAlignment">When PipelineLength is larger than 1 (SMP Pipelining Enabled) it's necessary to set this in order for the stack
+        /// <param name="byteAlignment">(iOS only) When PipelineLength is larger than 1 (SMP Pipelining Enabled) it's necessary to set this in order for the stack
         ///     to predict offset jumps as multiple packets are sent in parallel.</param>
         /// <param name="timeoutInMs">The amount of time to wait for the operation to complete before bailing out. If set to zero or negative then the operation will wait indefinitely.</param>
         Task InstallAsync(
@@ -47,7 +47,7 @@ namespace Laerdal.McuMgr.FirmwareInstaller.Contracts
         /// <param name="eraseSettings">Specifies whether preexisting settings should be erased or not.</param>
         /// <param name="estimatedSwapTimeInMilliseconds">In rF52840, due to how the flash memory works, requires ~20 sec to erase images.
         /// For Laerdal AEDs the recommended time is ~50secs. Adjust the time accordingly for each device you're testing.</param>
-        /// <param name="windowCapacity">Set the window capacity. Values > 1 enable a new implementation for uploading
+        /// <param name="windowCapacity">(Android only) Set the window capacity. Values > 1 enable a new implementation for uploading
         ///     the images, which makes use of SMP pipelining feature. The app will send this many packets immediately, without waiting for notification
         ///     confirming each packet. This value should be lower or equal to MCUMGR_BUF_COUNT
         ///     (https://github.com/zephyrproject-rtos/zephyr/blob/bd4ddec0c8c822bbdd420bd558b62c1d1a532c16/subsys/mgmt/mcumgr/Kconfig#L550)
@@ -55,11 +55,11 @@ namespace Laerdal.McuMgr.FirmwareInstaller.Contracts
         ///     before https://github.com/zephyrproject-rtos/zephyr/pull/41959 was merged, the device required data to be sent with memory alignment.
         ///     Otherwise, the device would ignore uneven bytes and reply with lower than expected offset
         ///     causing multiple packets to be sent again dropping the speed instead of increasing it.</param>
-        /// <param name="memoryAlignment">Set the selected memory alignment. Defaults to 4 to match Nordic devices.</param>
-        /// <param name="pipelineDepth">If set to a value larger than 1, this enables SMP Pipelining, wherein multiple packets of data ('chunks') are sent at
-        /// once before awaiting a response, which can lead to a big increase in transfer speed if the receiving hardware supports this feature.</param>
-        /// <param name="byteAlignment">When PipelineLength is larger than 1 (SMP Pipelining Enabled) it's necessary to set this in order for the stack
-        /// to predict offset jumps as multiple packets are sent in parallel.</param>
+        /// <param name="memoryAlignment">(Android only) Set the selected memory alignment. Defaults to 4 to match Nordic devices.</param>
+        /// <param name="pipelineDepth">(iOS only) If set to a value larger than 1, this enables SMP Pipelining, wherein multiple packets of data ('chunks') are sent at
+        ///     once before awaiting a response, which can lead to a big increase in transfer speed if the receiving hardware supports this feature.</param>
+        /// <param name="byteAlignment">(iOS only) When PipelineLength is larger than 1 (SMP Pipelining Enabled) it's necessary to set this in order for the stack
+        ///     to predict offset jumps as multiple packets are sent in parallel.</param>
         EFirmwareInstallationVerdict BeginInstallation(
             byte[] data,
             EFirmwareInstallationMode mode = EFirmwareInstallationMode.TestAndConfirm,
