603de7f34f733b5fb9e0cfe66a959c09f23c9cc0
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2023-08-24 15:59:20 +0200

    refa (Android/FirmwareInstaller): refactor the proxy to conform to the new mechanism

--- Laerdal.McuMgr/Droid/FirmwareInstaller/FirmwareInstaller.cs
+++ Laerdal.McuMgr/Droid/FirmwareInstaller/FirmwareInstaller.cs
@@ -10,176 +10,208 @@ using Laerdal.Java.McuMgr.Wrapper.Android;
 using Laerdal.McuMgr.Common;
 using Laerdal.McuMgr.FirmwareInstaller.Contracts;
 using Laerdal.McuMgr.FirmwareInstaller.Contracts.Enums;
-using Laerdal.McuMgr.FirmwareInstaller.Contracts.Events;
+using Laerdal.McuMgr.FirmwareInstaller.Contracts.Native;
 
 namespace Laerdal.McuMgr.FirmwareInstaller
 {
     /// <inheritdoc cref="IFirmwareInstaller"/>
     public partial class FirmwareInstaller : IFirmwareInstaller
     {
-        private readonly AndroidFirmwareInstallerProxy _androidFirmwareInstallerProxy;
-
-        public FirmwareInstaller(BluetoothDevice bluetoothDevice, Context androidContext = null)
+        public FirmwareInstaller(BluetoothDevice bluetoothDevice, Context androidContext = null) : this(ValidateArgumentsAndConstructProxy(bluetoothDevice, androidContext))
         {
-            if (bluetoothDevice == null)
-                throw new ArgumentNullException(nameof(bluetoothDevice));
+        }
+
+        static private INativeFirmwareInstallerProxy ValidateArgumentsAndConstructProxy(BluetoothDevice bluetoothDevice, Context androidContext = null)
+        {
+            bluetoothDevice = bluetoothDevice ?? throw new ArgumentNullException(nameof(bluetoothDevice));
 
             androidContext ??= Application.Context;
             if (androidContext == null)
                 throw new InvalidOperationException("Failed to retrieve the Android Context in which this call takes place - this is weird");
 
-            _androidFirmwareInstallerProxy = new AndroidFirmwareInstallerProxy(this, androidContext, bluetoothDevice);
+            return new AndroidFirmwareInstallerProxy(
+                context: androidContext,
+                bluetoothDevice: bluetoothDevice,
+                firmwareInstallerCallbacksProxy: new GenericNativeFirmwareInstallerCallbacksProxy()
+            );
         }
 
-        // public string LastFatalErrorMessage => _androidFirmwareInstallerProxy?.LastFatalErrorMessage;
-        //
-        // public EFirmwareInstallationVerdict BeginInstallation(
-        //     byte[] data,
-        //     EFirmwareInstallationMode mode = EFirmwareInstallationMode.TestAndConfirm,
-        //     bool? eraseSettings = null,
-        //     int? estimatedSwapTimeInMilliseconds = null,
-        //     int? windowCapacity = null, //   android only    not applicable for ios
-        //     int? memoryAlignment = null, //  android only    not applicable for ios
-        //     int? pipelineDepth = null, //    ios only        not applicable for android
-        //     int? byteAlignment = null //     ios only        not applicable for android
-        // )
-        // {
-        //     if (data == null || !data.Any())
-        //         throw new ArgumentException("The data byte-array parameter is null or empty", nameof(data));
-        //
-        //     _androidFirmwareInstallerProxy.Nickname = "Firmware Installation"; //todo  get this from a parameter 
-        //     var verdict = _androidFirmwareInstallerProxy.BeginInstallation(
-        //         data: data,
-        //         mode: TranslateFirmwareInstallationMode(mode),
-        //         eraseSettings: eraseSettings ?? false,
-        //         windowCapacity: windowCapacity ?? -1,
-        //         memoryAlignment: memoryAlignment ?? -1,
-        //         estimatedSwapTimeInMilliseconds: estimatedSwapTimeInMilliseconds ?? -1
-        //         // pipelineDepth: not applicable in android
-        //         // byteAlignment: not applicable in android
-        //     );
-        //
-        //     return TranslateFirmwareInstallationVerdict(verdict);
-        // }
-        //
-        // public void Cancel() => _androidFirmwareInstallerProxy.Cancel();
-        // public void Disconnect() => _androidFirmwareInstallerProxy.Disconnect();
-
-        static private EAndroidFirmwareInstallationMode TranslateFirmwareInstallationMode(EFirmwareInstallationMode mode) => mode switch
-        {
-            EFirmwareInstallationMode.TestOnly => EAndroidFirmwareInstallationMode.TestOnly, //0
-            EFirmwareInstallationMode.ConfirmOnly => EAndroidFirmwareInstallationMode.ConfirmOnly,
-            EFirmwareInstallationMode.TestAndConfirm => EAndroidFirmwareInstallationMode.TestAndConfirm,
-            _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
-
-            //0 we have to separate enums
-            //
-            //  - EFirmwareInstallationMode which is publicly exposed and used by both android and ios
-            //  - EAndroidFirmwareInstallationMode which is specific to android and should not be used by the api surface or the end users  
-        };
-
-        static private EFirmwareInstallationVerdict TranslateFirmwareInstallationVerdict(EAndroidFirmwareInstallationVerdict verdict)
-        {
-            if (verdict == EAndroidFirmwareInstallationVerdict.Success) //0
-            {
-                return EFirmwareInstallationVerdict.Success;
-            }
-
-            if (verdict == EAndroidFirmwareInstallationVerdict.FailedDeploymentError)
-            {
-                return EFirmwareInstallationVerdict.FailedDeploymentError;
-            }
-
-            if (verdict == EAndroidFirmwareInstallationVerdict.FailedInvalidSettings)
-            {
-                return EFirmwareInstallationVerdict.FailedInvalidSettings;
-            }
-
-            if (verdict == EAndroidFirmwareInstallationVerdict.FailedInvalidDataFile)
-            {
-                return EFirmwareInstallationVerdict.FailedInvalidDataFile;
-            }
-            
-            if (verdict == EAndroidFirmwareInstallationVerdict.FailedInstallationAlreadyInProgress)
-            {
-                return EFirmwareInstallationVerdict.FailedInstallationAlreadyInProgress;
-            }
-
-            throw new ArgumentOutOfRangeException(nameof(verdict), verdict, null);
-
-            //0 we have to separate enums
-            //
-            //  - EFirmwareInstallationVerdict which is publicly exposed and used by both android and ios
-            //  - EAndroidFirmwareInstallationVerdict which is specific to android and should not be used by the api surface or the end users
-        }
-
-        private sealed class AndroidFirmwareInstallerProxy : AndroidFirmwareInstaller
+        private sealed class AndroidFirmwareInstallerProxy : AndroidFirmwareInstaller, INativeFirmwareInstallerProxy
         {
             public string Nickname { get; set; }
+
+            private readonly INativeFirmwareInstallerCallbacksProxy _firmwareInstallerCallbacksProxy;
             
-            private readonly FirmwareInstaller _installer;
+            public IFirmwareInstallerEventEmittable FirmwareInstaller //keep this to conform to the interface
+            {
+                get => _firmwareInstallerCallbacksProxy?.FirmwareInstaller;
+                set
+                {
+                    if (_firmwareInstallerCallbacksProxy == null)
+                        return;
+
+                    _firmwareInstallerCallbacksProxy.FirmwareInstaller = value;
+                }
+            }
 
             // ReSharper disable once UnusedMember.Local
             private AndroidFirmwareInstallerProxy(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
             {
             }
 
-            internal AndroidFirmwareInstallerProxy(FirmwareInstaller installer, Context context, BluetoothDevice bluetoothDevice) : base(context, bluetoothDevice)
+            internal AndroidFirmwareInstallerProxy(INativeFirmwareInstallerCallbacksProxy firmwareInstallerCallbacksProxy, Context context, BluetoothDevice bluetoothDevice) : base(context, bluetoothDevice)
             {
-                _installer = installer ?? throw new ArgumentNullException(nameof(installer));
+                _firmwareInstallerCallbacksProxy = firmwareInstallerCallbacksProxy ?? throw new ArgumentNullException(nameof(firmwareInstallerCallbacksProxy));
+            }
+
+            public EFirmwareInstallationVerdict BeginInstallation(
+                byte[] data,
+                EFirmwareInstallationMode mode = EFirmwareInstallationMode.TestAndConfirm,
+                bool? eraseSettings = null,
+                int? estimatedSwapTimeInMilliseconds = null,
+                int? windowCapacity = null,
+                int? memoryAlignment = null,
+                int? pipelineDepth = null, // ignored in android   it only affects ios
+                int? byteAlignment = null //  ignored in android   it only affects ios
+            )
+            {
+                var nativeVerdict = base.BeginInstallation(
+                    data: data,
+                    mode: TranslateFirmwareInstallationMode(mode),
+                    eraseSettings: eraseSettings ?? false,
+                    windowCapacity: windowCapacity ?? -1,
+                    memoryAlignment: memoryAlignment ?? -1,
+                    estimatedSwapTimeInMilliseconds: estimatedSwapTimeInMilliseconds ?? -1
+                    // pipelineDepth: ignored in android   it only affects ios
+                    // byteAlignment: ignored in android   it only affects ios
+                );
+
+                return TranslateFirmwareInstallationVerdict(nativeVerdict);
             }
 
             public override void FatalErrorOccurredAdvertisement(string errorMessage)
             {
                 base.FatalErrorOccurredAdvertisement(errorMessage);
 
-                _installer.OnFatalErrorOccurred(new FatalErrorOccurredEventArgs(errorMessage));
+                _firmwareInstallerCallbacksProxy?.FatalErrorOccurredAdvertisement(errorMessage);
             }
             
             public override void LogMessageAdvertisement(string message, string category, string level)
             {
                 base.LogMessageAdvertisement(message, category, level);
 
-                _installer.OnLogEmitted(new LogEmittedEventArgs(
+                LogMessageAdvertisement(
                     level: HelpersAndroid.TranslateEAndroidLogLevel(level),
                     message: message,
                     category: category,
                     resource: Nickname
-                ));
+                );
+            }
+
+            public void LogMessageAdvertisement(string message, string category, ELogLevel level, string resource)
+            {
+                _firmwareInstallerCallbacksProxy?.LogMessageAdvertisement(
+                    level: level,
+                    message: message,
+                    category: category,
+                    resource: resource
+                );
             }
 
             public override void CancelledAdvertisement()
             {
                 base.CancelledAdvertisement(); //just in case
-                
-                _installer.OnCancelled(new CancelledEventArgs());
+
+                _firmwareInstallerCallbacksProxy?.CancelledAdvertisement();
             }
 
             public override void BusyStateChangedAdvertisement(bool busyNotIdle)
             {
                 base.BusyStateChangedAdvertisement(busyNotIdle); //just in case
-                
-                _installer.OnBusyStateChanged(new BusyStateChangedEventArgs(busyNotIdle));
+
+                _firmwareInstallerCallbacksProxy?.BusyStateChangedAdvertisement(busyNotIdle);
             }
 
             public override void StateChangedAdvertisement(EAndroidFirmwareInstallationState oldState, EAndroidFirmwareInstallationState newState)
             {
                 base.StateChangedAdvertisement(oldState, newState); //just in case
 
-                _installer.OnStateChanged(new StateChangedEventArgs(
+                StateChangedAdvertisement(
                     newState: TranslateEAndroidFirmwareInstallationState(newState),
                     oldState: TranslateEAndroidFirmwareInstallationState(oldState)
-                ));
+                );
             }
 
+            public void StateChangedAdvertisement(EFirmwareInstallationState oldState, EFirmwareInstallationState newState)
+            {
+                _firmwareInstallerCallbacksProxy?.StateChangedAdvertisement(newState: newState, oldState: oldState);
+            }
+
+            //todo   rename this to FirmwareUploadProgressPercentageAndDataThroughputChangedAdvertisement
             public override void FirmwareUploadProgressPercentageAndThroughputDataChangedAdvertisement(int progressPercentage, float averageThroughput)
             {
                 base.FirmwareUploadProgressPercentageAndThroughputDataChangedAdvertisement(progressPercentage, averageThroughput); //just in case
 
-                _installer.OnFirmwareUploadProgressPercentageAndThroughputDataChanged(new FirmwareUploadProgressPercentageAndDataThroughputChangedEventArgs(
+                FirmwareUploadProgressPercentageAndDataThroughputChangedAdvertisement(
                     averageThroughput: averageThroughput,
                     progressPercentage: progressPercentage
-                ));
+                );
+            }
+
+            public void FirmwareUploadProgressPercentageAndDataThroughputChangedAdvertisement(int progressPercentage, float averageThroughput)
+            {
+                _firmwareInstallerCallbacksProxy?.FirmwareUploadProgressPercentageAndDataThroughputChangedAdvertisement(
+                    averageThroughput: averageThroughput,
+                    progressPercentage: progressPercentage
+                );
+            }
+
+            static private EAndroidFirmwareInstallationMode TranslateFirmwareInstallationMode(EFirmwareInstallationMode mode) => mode switch
+            {
+                EFirmwareInstallationMode.TestOnly => EAndroidFirmwareInstallationMode.TestOnly, //0
+                EFirmwareInstallationMode.ConfirmOnly => EAndroidFirmwareInstallationMode.ConfirmOnly,
+                EFirmwareInstallationMode.TestAndConfirm => EAndroidFirmwareInstallationMode.TestAndConfirm,
+                _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
+
+                //0 we have to separate enums
+                //
+                //  - EFirmwareInstallationMode which is publicly exposed and used by both android and ios
+                //  - EAndroidFirmwareInstallationMode which is specific to android and should not be used by the api surface or the end users  
+            };
+
+            static private EFirmwareInstallationVerdict TranslateFirmwareInstallationVerdict(EAndroidFirmwareInstallationVerdict verdict)
+            {
+                if (verdict == EAndroidFirmwareInstallationVerdict.Success) //0
+                {
+                    return EFirmwareInstallationVerdict.Success;
+                }
+
+                if (verdict == EAndroidFirmwareInstallationVerdict.FailedDeploymentError)
+                {
+                    return EFirmwareInstallationVerdict.FailedDeploymentError;
+                }
+
+                if (verdict == EAndroidFirmwareInstallationVerdict.FailedInvalidSettings)
+                {
+                    return EFirmwareInstallationVerdict.FailedInvalidSettings;
+                }
+
+                if (verdict == EAndroidFirmwareInstallationVerdict.FailedInvalidDataFile)
+                {
+                    return EFirmwareInstallationVerdict.FailedInvalidDataFile;
+                }
+
+                if (verdict == EAndroidFirmwareInstallationVerdict.FailedInstallationAlreadyInProgress)
+                {
+                    return EFirmwareInstallationVerdict.FailedInstallationAlreadyInProgress;
+                }
+
+                throw new ArgumentOutOfRangeException(nameof(verdict), verdict, null);
+
+                //0 we have to separate enums
+                //
+                //  - EFirmwareInstallationVerdict which is publicly exposed and used by both android and ios
+                //  - EAndroidFirmwareInstallationVerdict which is specific to android and should not be used by the api surface or the end users
             }
 
             static private EFirmwareInstallationState TranslateEAndroidFirmwareInstallationState(EAndroidFirmwareInstallationState state)
@@ -188,7 +220,7 @@ namespace Laerdal.McuMgr.FirmwareInstaller
                 {
                     return EFirmwareInstallationState.None;
                 }
-                
+
                 if (state == EAndroidFirmwareInstallationState.Idle)
                 {
                     return EFirmwareInstallationState.Idle;
@@ -228,12 +260,12 @@ namespace Laerdal.McuMgr.FirmwareInstaller
                 {
                     return EFirmwareInstallationState.Complete;
                 }
-                
+
                 if (state == EAndroidFirmwareInstallationState.Cancelled)
                 {
                     return EFirmwareInstallationState.Cancelled;
                 }
-                
+
                 if (state == EAndroidFirmwareInstallationState.Error)
                 {
                     return EFirmwareInstallationState.Error;
