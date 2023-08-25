3642887886f739edf0603b3d8b6d3753a3b9442e
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2023-08-25 19:54:08 +0200

    refa (EAndroidDeviceResetterState, EAndroidFirmwareEraserState): make the enum values explicit

--- Laerdal.McuMgr.Bindings.Android/mcumgr-laerdal-wrapper/src/main/java/no/laerdal/mcumgr_laerdal_wrapper/EAndroidDeviceResetterState.java
+++ Laerdal.McuMgr.Bindings.Android/mcumgr-laerdal-wrapper/src/main/java/no/laerdal/mcumgr_laerdal_wrapper/EAndroidDeviceResetterState.java
@@ -1,10 +1,17 @@
 package no.laerdal.mcumgr_laerdal_wrapper;
 
 public enum EAndroidDeviceResetterState {
-    NONE,
-    IDLE,
-    RESETTING,
-    COMPLETE,
-    FAILED
-}
+    NONE(0),
+    IDLE(1),
+    RESETTING(2),
+    COMPLETE(3),
+    FAILED(4);
 
+    @SuppressWarnings({"FieldCanBeLocal", "unused"})
+    private final int _value;
+
+    EAndroidDeviceResetterState(int value)
+    {
+        _value = value;
+    }
+}
--- Laerdal.McuMgr.Bindings.Android/mcumgr-laerdal-wrapper/src/main/java/no/laerdal/mcumgr_laerdal_wrapper/EAndroidFirmwareEraserState.java
+++ Laerdal.McuMgr.Bindings.Android/mcumgr-laerdal-wrapper/src/main/java/no/laerdal/mcumgr_laerdal_wrapper/EAndroidFirmwareEraserState.java
@@ -1,9 +1,17 @@
 package no.laerdal.mcumgr_laerdal_wrapper;
 
 public enum EAndroidFirmwareEraserState {
-    NONE,
-    IDLE,
-    ERASING,
-    COMPLETE,
-    FAILED
+    NONE(0),
+    IDLE(1),
+    ERASING(2),
+    COMPLETE(3),
+    FAILED(4);
+
+    @SuppressWarnings({"FieldCanBeLocal", "unused"})
+    private final int _value;
+
+    EAndroidFirmwareEraserState(int value)
+    {
+        _value = value;
+    }
 }
