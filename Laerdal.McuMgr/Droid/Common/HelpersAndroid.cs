3d920d33a4a31ad63c62b2d69cfbb3145acf0d21
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2023-08-25 14:56:33 +0200

    refa (Translate*LogLevel): make the methods case-insensitive

--- Laerdal.McuMgr/Droid/Common/HelpersAndroid.cs
+++ Laerdal.McuMgr/Droid/Common/HelpersAndroid.cs
@@ -4,7 +4,7 @@ namespace Laerdal.McuMgr.Common
 {
     static internal class HelpersAndroid
     {
-        static public ELogLevel TranslateEAndroidLogLevel(string level) => level switch //derived from sl4j https://www.slf4j.org/api/org/apache/log4j/Level.html
+        static public ELogLevel TranslateEAndroidLogLevel(string level) => level?.Trim().ToUpperInvariant() switch //derived from sl4j https://www.slf4j.org/api/org/apache/log4j/Level.html
         {
             "DEBUG" => ELogLevel.Debug,
             "TRACE" => ELogLevel.Verbose,
@@ -12,7 +12,7 @@ namespace Laerdal.McuMgr.Common
             "FATAL" => ELogLevel.Error,
             "WARN" => ELogLevel.Warning,
             "ERROR" => ELogLevel.Error,
-            _ => throw new ArgumentOutOfRangeException(nameof(level), level, "Unknown enum value")
+            _ => throw new ArgumentOutOfRangeException(nameof(level), level, "Unknown log-level value")
         };
     }
 }
