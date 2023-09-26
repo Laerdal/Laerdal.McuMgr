55f2b6a17c024ebf5f0b6d27ce3844d35cf727d9
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2023-09-26 17:51:09 +0200

    feat (FirmwareInstallationConfirmationStageTimeoutException): refine the exception message

--- Laerdal.McuMgr/Shared/FirmwareInstaller/Contracts/Exceptions/FirmwareInstallationConfirmationStageTimeoutException.cs
+++ Laerdal.McuMgr/Shared/FirmwareInstaller/Contracts/Exceptions/FirmwareInstallationConfirmationStageTimeoutException.cs
@@ -4,8 +4,8 @@ namespace Laerdal.McuMgr.FirmwareInstaller.Contracts.Exceptions
 {
     public class FirmwareInstallationConfirmationStageTimeoutException : FirmwareInstallationErroredOutException, IFirmwareInstallationException
     {
-        public FirmwareInstallationConfirmationStageTimeoutException()
-            : base("Device didn't confirm the new firmware within the given timeframe")
+        public FirmwareInstallationConfirmationStageTimeoutException(int? estimatedSwapTimeInMilliseconds)
+            : base($"Device didn't confirm the new firmware within the given timeframe of {estimatedSwapTimeInMilliseconds} milliseconds. The new firmware will only last for just one power-cycle of the device.")
         {
         }
     }
