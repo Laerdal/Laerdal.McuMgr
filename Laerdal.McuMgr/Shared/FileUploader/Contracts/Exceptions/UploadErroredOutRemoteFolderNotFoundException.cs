820cb73dc898a2827e22a00eca8746b11fdb37b0
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2024-01-16 15:18:34 +0100

    refa (UploadErroredOutRemoteFolderNotFoundException.cs): add a new property called 'RemoteFolderPath' that holds the path of the non-existent remote folder

--- Laerdal.McuMgr/Shared/FileUploader/Contracts/Exceptions/UploadErroredOutRemoteFolderNotFoundException.cs
+++ Laerdal.McuMgr/Shared/FileUploader/Contracts/Exceptions/UploadErroredOutRemoteFolderNotFoundException.cs
@@ -4,8 +4,11 @@ namespace Laerdal.McuMgr.FileUploader.Contracts.Exceptions
 {
     public sealed class UploadErroredOutRemoteFolderNotFoundException : UploadErroredOutException, IUploadException
     {
+        public string RemoteFolderPath { get; }
+        
         public UploadErroredOutRemoteFolderNotFoundException(string remoteFolderPath) : base($"Remote folder-path '{remoteFolderPath}' doesn't exist")
         {
+            RemoteFolderPath = remoteFolderPath;
         }
     }
 }
