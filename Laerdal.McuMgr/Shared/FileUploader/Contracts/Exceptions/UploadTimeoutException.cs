e3a49900342ed933a1e2c43e3e83214c0a575392
D. Sidiropoulos <dominick.sidiropoulos@gmail.com>
2024-01-16 15:49:16 +0100

    refa (UploadTimeoutException.cs): add a new property .RemoteFilePath which holds the filepath of the asset whose upload timed out

--- Laerdal.McuMgr/Shared/FileUploader/Contracts/Exceptions/UploadTimeoutException.cs
+++ Laerdal.McuMgr/Shared/FileUploader/Contracts/Exceptions/UploadTimeoutException.cs
@@ -6,9 +6,12 @@ namespace Laerdal.McuMgr.FileUploader.Contracts.Exceptions
 {
     public sealed class UploadTimeoutException : UploadErroredOutException, IUploadException
     {
-        public UploadTimeoutException(string remoteFolderPath, int timeoutInMs, Exception innerException)
-            : base($"Failed to upload over to '{remoteFolderPath}' on the device within {timeoutInMs}ms", innerException)
+        public string RemoteFilePath { get; }
+        
+        public UploadTimeoutException(string remoteFilePath, int timeoutInMs, Exception innerException)
+            : base($"Failed to upload over to '{remoteFilePath}' on the device within {timeoutInMs}ms", innerException)
         {
+            RemoteFilePath = remoteFilePath;
         }
     }
 }
