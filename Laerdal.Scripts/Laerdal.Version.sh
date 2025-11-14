4c20a8a6fc4a677ced51a2082e4d072a79b5d805
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2025-11-14 18:05:40 +0100

    fix (Laerdal.Version.sh): we now suppress any errors/warnings from 'git fetch'; this solves a bug which was causing the output of the script to be mangled in our CI/CD environment

--- Laerdal.Scripts/Laerdal.Version.sh
+++ Laerdal.Scripts/Laerdal.Version.sh
@@ -5,6 +5,8 @@
 # -x: log all commands
 # -o pipefail: entire command fails if pipe fails. watch out for yes | ...
 # -o history: record shell history
+
+# set -xo pipefail -o history
 # set -exo pipefail -o history
 
 # This script is used to generate a version number for a release
@@ -98,7 +100,8 @@ function escape_regex_string() {
 }
 
 # Check if git branch -r contains develop
-git fetch --all
+git fetch --all --quiet >/dev/null 2>&1 || true
+
 if git branch -r | grep -q "^\s*$(escape_regex_string "$remote_name/develop")$"; then
     develop_branch="develop"
     info "develop_branch=$develop_branch (git branch -r)"
