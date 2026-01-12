c4786277582850e2709dbb985aa5525d63d45589
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2026-01-12 19:12:11 +0100

    clean (TaskExtensions.swift): move the task-extensions to their own separate file

--- Laerdal.McuMgr.Bindings.MacCatalystAndIos.Native/McuMgrBindingsiOS/McuMgrBindingsiOS/McuMgrLogLevelHelpers.swift
+++ Laerdal.McuMgr.Bindings.MacCatalystAndIos.Native/McuMgrBindingsiOS/McuMgrBindingsiOS/McuMgrLogLevelHelpers.swift
@@ -20,16 +20,4 @@ internal class McuMgrLogLevelHelpers {
     }
 }
 
-internal extension Task where Success == Void, Failure == Never { //we extend the 'Task' class      todo   move this to its own file
-    static func fireAndForgetInTheBg(
-        _ operation: @escaping @Sendable () -> Void
-    ) {
-        Task<Void, Never>.detached(priority: .background) {
-            do {
-                operation()
-            } catch {
-                // logger?.warning("Fire-and-forget task failed: \(error)")
-            }
-        }
-    }
-}
+
